1bb929bcec45f65eddfe6d1273de70c906fd0cd3
gomills <gomills@protonmail.com>
2025-12-27 17:43:24 +0100

    FIxed, from robots, the pressumption that there were one : per line

--- pkg/urls_extractors/crawl_robots.go
+++ pkg/urls_extractors/crawl_robots.go
@@ -13,7 +13,7 @@ import (
 // extractUrlsFromRobots performs a URL extraction from robots by parsing it. This functions extracts ALL URLs, it doesn't respect ROBOTS.txt at all.
 // In fact, it does the opposite
 func extractUrlsFromRobots(ctx context.Context, bodyStr string, config *config.Config, qp *queue.Queue, domain string, registeredDomain string) {
-	scanner := bufio.NewScanner(strings.NewReader(string(bodyStr)))
+	scanner := bufio.NewScanner(strings.NewReader(bodyStr))
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -25,7 +25,7 @@ func extractUrlsFromRobots(ctx context.Context, bodyStr string, config *config.C
 
 		cleanLine = strings.ToLower(cleanLine)
 		if strings.HasPrefix(cleanLine, "disallow:") || strings.HasPrefix(cleanLine, "allow:") || strings.HasPrefix(cleanLine, "sitemap:") {
-			values := strings.Split(cleanLine, ":")
+			values := strings.SplitN(cleanLine, ":", 2)
 			if len(values) == 1 {
 				continue
 			}
