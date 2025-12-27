92d9cffaf518d5b138f00a06e655a4f4ff4131f5
gomills <gomills@protonmail.com>
2025-12-27 17:44:24 +0100

    Made the sitemap identification more precise

--- pkg/urls_extractors/crawl_sitemap.go
+++ pkg/urls_extractors/crawl_sitemap.go
@@ -13,7 +13,9 @@ import (
 // CrawlXml regexes sitemap.xml for urls.
 func CrawlXml(ctx context.Context, config *config.Config, parsedUrl *url.URL, response *http.Response, qp *queue.Queue, domain string, registeredDomain string) {
 
-	if strings.Contains(parsedUrl.Path, "sitemap.xml") {
+	trimmedPath := strings.Trim(parsedUrl.Path, "/")
+
+	if strings.HasSuffix(trimmedPath, "sitemap.xml") {
 		regexFileForUrls(ctx, config, response, qp, domain, registeredDomain)
 
 	}
