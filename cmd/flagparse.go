da784d55ffb78305cb65fde0b181e51f42b051bd
gomills <gomills@protonmail.com>
2026-06-08 08:57:34 +0200

    Refactored flag generation. Improved for more sane defaults, sentinel values to some values and more info now when using -h

--- cmd/flagparse.go
+++ cmd/flagparse.go
@@ -8,30 +8,36 @@ import (
 	"github.com/gomills/millcrawler/pkg/config"
 )
 
-// parseFlagsIntoConfig parses flags from the CLI and instantiates a new config for the crawler. A friendly user is expected to call this function.
-// In other words, it doesn't have unbreakable validation, just basic.
+const (
+	defaultAllowedExtDomains = "github.com,gitlab.com,docker.com,pastebin.com,nuget.org,bitbucket.org,s3.amazonaws.com,telegram.org,slack.com,drive.google.com,docs.google.com,codeberg.org"
+	defaultAllowedExtensions = ",.js,.ts,.map,.properties,.log,.cfg,.pem,.crt,.npmrc,.yarnrc,.html,.htm,.json,.yaml,.yml,.env,.conf,.xml,.txt,.py,.php,.git,.sh,.key,.go,.ini,.example,.md,.rb,.java,.cpp,.c,.pl,.zsh,.bak,.old,.sql,.db,.tar,.gz,.zip,.ovpn,.toml"
+)
+
+// parseFlagsIntoConfig parses flags from the CLI and instantiates a new config for the crawler.
+// It supposes that a friendly user will call this function (it doesn't have unbreakable validation, just basic).
 func parseFlagsIntoConfig() (string, *config.Config, error) {
 
 	// load config from flags
-	domain := flag.String("domain", "", "Target domain to crawl. Example: 'example.com'")
-	timeOutDuration := flag.Int("timeoutseconds", 60, "Maximum crawl duration in seconds. Example: '60'")
-	maxPathDepth := flag.Int("maxpathdepth", 1, "Maximum URL path depth to crawl. Example: '2' allows '/path1/path2'")
-	maxNCrawledUrls := flag.Int("maxnumurls", 100, "Maximum number of URLs to crawl before stopping")
-	allowedExternalDomains := flag.String("allowedextdomains", "", "Comma-separated list of allowed external domains. Example: 'github.com,gitlab.com'")
-	sensitivePatterns := flag.String("sensitivepatterns", "", "Comma-separated URL patterns that bypass max path depth restrictions. Example: 'api,admin,debug'")
-	allowedExtensions := flag.String("allowedextensions", ",.htm,.html,.js", "Comma-separated list of allowed file extensions. Example: '.html,.js,.json'")
-	workers := flag.Int("workers", 1, "Number of concurrent crawling workers")
-	printUrls := flag.Bool("printurls", false, "Interactive logging: banner, URLs, to stdout.")
-	bruteforceSubdomains := flag.Bool("bruteforcesubdomains", true, "Enable subdomain bruteforcing. E.g: 'true' for testing.example.com, admin.example.com, ...")
-	scanSecrets := flag.Bool("scansecrets", false, "Decide if scan responses for secrets")
-	cookies := flag.Bool("cookies", false, "Enable cookie jar for use during crawling")
+	domain := flag.String("domain", "", "Target domain to crawl. Example: -domain=example.com")
+	timeOutDuration := flag.Int("timeoutseconds", 3600, "Maximum crawl duration in seconds, never unlimited. Example: -timeoutseconds=60")
+	maxPathDepth := flag.Int("maxpathdepth", 0, "Maximum URL path depth to crawl, 0 for unlimited. Example: -maxpathdepth=2 allows /path1/path2 but not /path1/path2/path3")
+	maxNCrawledUrls := flag.Int("maxnumurls", 0, "Maximum number of URLs to crawl before stopping, 0 for unlimited. Example: -maxnumurls=400")
+	allowedExternalDomains := flag.String("allowedextdomains", defaultAllowedExtDomains, "Comma-separated list of allowed external domains. Example: -allowedextdomains=github.com,gitlab.com")
+	sensitivePatterns := flag.String("sensitivepatterns", "", "Comma-separated URL patterns that bypass max path depth restrictions. Example: sensitivepatterns=api,admin,debug")
+	allowedExtensions := flag.String("allowedextensions", defaultAllowedExtensions, "Comma-separated list of allowed file extensions ('' for subpages...). Example: allowedextensions=,.html,.js,.json")
+	workers := flag.Int("workers", 1, "Number of concurrent crawling workers. Example: -workers=10")
+	printUrls := flag.Bool("printurls", false, "Interactive logging: banner and URLs to stdout. If disabled, it outputs .json results directly for scripting integration. Example: -printurls=true")
+	bruteforceSubdomains := flag.Bool("bruteforcesubdomains", false, "Enable subdomain bruteforcing. Example: -bruteforcesubdomains=true")
+	scanSecrets := flag.Bool("scansecrets", false, "Decide if scan responses for secrets. Example: -scansecrets=true")
+	cookies := flag.Bool("cookies", false, "Enable cookie jar for use during crawling. Example: -cookies=true")
 	flag.Parse()
 
-	// manually validate the only value not used for config where there's native validation
+	// manually validate the only value not used for config (in config there's native validation already...)
 	if *domain == "" {
 		return "", nil, errors.New("Domain is empty")
 	}
 
+	// instantiate config and check for error where there's validation for the rest of the input parameters
 	configuration, err := config.LoadConfig(
 		*timeOutDuration,
 		*maxPathDepth,
